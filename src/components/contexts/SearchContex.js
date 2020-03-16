9aff92c9ff6939182c5614136e26ba9f392b8176
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-03-16 14:47:25 +0300

    create search context methods

--- src/components/contexts/SearchContex.js
+++ src/components/contexts/SearchContex.js
@@ -1,15 +1,26 @@
 import React, { createContext } from 'react';
+const axios = require('axios');
 
 export const SearchContext = createContext();
 
 class SearchContextProvider extends React.Component {
   state = {};
 
+  /**
+   * check localstorage first for given query,
+   * return results from localstorage if already exist
+   */
+  retrieveFromCache = () => {};
+
+  // retrieve search results from Unsplash api
+  retrieveFromApi = () => {};
+
   render() {
     return (
       <SearchContext.Provider
         value={{
-          ...this.state
+          ...this.state,
+          search: this.retrieveFromApi
         }}
       >
         {this.props.children}
