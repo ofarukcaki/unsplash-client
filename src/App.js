32c750d03f009fa11706dff0dd21657375c9fb8e
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-03-16 13:22:55 +0300

    container and grid css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import MasonryGrid from './components/MasonryGrid';
+
+const collectionList = [
+  'featured',
+  'walpapers',
+  'nature',
+  'textures & patterns',
+  'architecture'
+];
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MasonryGrid c={collectionList}/>
     </div>
   );
 }
