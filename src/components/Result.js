ac452116c63aa0e2d9e17927636bdd6419bf4900
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-03-16 19:28:29 +0300

    added svg loader

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,11 @@ import MasonryGrid from './MasonryGrid';
 import { SearchContext } from '../contexts/SearchContex';
 
 function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <div className="loader">
+      <SvgLoader />
+    </div>
+  );
 }
 
 function Result() {
@@ -14,10 +18,104 @@ function Result() {
     return <p>No results</p>;
   } else if (loading) {
     return <Loading />;
-    return;
   } else {
     return <MasonryGrid results={results} />;
   }
+}
+
+function SvgLoader(props) {
+  return (
+    <svg width={58} height={58} viewBox="0 0 58 58" {...props}>
+      <g
+        transform="translate(2 1)"
+        stroke="#FFF"
+        strokeWidth={1.5}
+        fill="none"
+        fillRule="evenodd"
+      >
+        <circle cx={42.601} cy={11.462} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="1;0;0;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={49.063} cy={27.063} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;1;0;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={42.601} cy={42.663} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;1;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={27} cy={49.125} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;1;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={11.399} cy={42.663} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;1;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={4.938} cy={27.063} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;1;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={11.399} cy={11.462} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;0;1;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={27} cy={5} r={5} fill="#000">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;0;0;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </g>
+    </svg>
+  );
 }
 
 export default Result;
