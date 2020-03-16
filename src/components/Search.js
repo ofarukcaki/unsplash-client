4849cbf4959313888b9eadec3cfae7880acd49c6
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-03-16 14:19:35 +0300

    convert Navbar to class component

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,31 +9,41 @@ const collectionList = [
   'architecture'
 ];
 
-function Navbar() {
-  return (
-    <form id="search">
-      <div>
-        <input className="query" type="text" placeholder="Query" />
-      </div>
-      <div>
-        <select id="collections" defaultValue="">
-          <option value="" disabled>
-            Collections
-          </option>
-          {collectionList.map(collectionItem => {
-            return (
-              <option key={collectionItem} value={collectionItem}>
-                {collectionItem}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <button id="search-btn">search</button>
-      </div>
-    </form>
-  );
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form id="search" onSubmit={this.onSubmit}>
+        <div>
+          <input className="query" type="text" placeholder="Query" />
+        </div>
+        <div>
+          <select id="collections" defaultValue="">
+            <option value="" disabled>
+              Collections
+            </option>
+            {collectionList.map(collectionItem => {
+              return (
+                <option key={collectionItem} value={collectionItem}>
+                  {collectionItem}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <button id="search-btn">search</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Navbar;
