ce5d5f8fedf36dfb63fc20ff6d6dcb36c6064423
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-03-18 21:32:20 +0300

    Create Navigation buttons

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <div id="navigation">
+      <button className="disabled">Previous</button>
+      <button>Next</button>
+    </div>
+  );
+}
+
+export default Navigation;
